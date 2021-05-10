e4fa189ce6f94ee2f7196dc6191586b746e6bde9
Kevin Huynh <kevinhuynh269@yahoo.com>
2021-05-10 13:49:51 -0500

    added the library and the using statements

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ApplicationStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var example = new exampleClass();
+            example.additionMethod();
+            Console.WriteLine("If this runs then addition Method works ");
+        }
+    }
+}
